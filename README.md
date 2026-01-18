c645e94e34988ccba95d1aa3ae42b00d7dc96bf1
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2026-01-18 13:48:23 -0800

    proof of concept done

---
+++ README.md
@@ -0,0 +1,27 @@
+# What is this?
+![Illustration of output of code](kermie.jpg)
+
+This is a python script/demo which takes a grayscale image and encodes it as a
+discrete parametric curve which gives an impression of the image based on 
+perceived density of lines.
+
+# To run
+To use, clone this repository and modify the script to load your image. 
+The code should display the figure (your results may vary based on your IDE).
+
+# Requirements
+* numpy (various array operations)
+* PIL (to load images)
+* matplotlib (to visualize)
+
+# TODO
+* Library of curves (other spirals; space-filling curves)
+* Address issues of bunching (linear spiral bunches at the origin)
+
+# Acknowledgments
+This was inspired by a random user post on imgur (possibly reddit) who was doing 
+something similar with a mechanical printing device about 8-10 years ago (as of Jan 2026).
+They might have been trying to turn it into an Etsy store or similar; not sure. 
+If you know who this is/was, or can find the post, please reach out to me. 
+If by chance this is your design and you want this taken down, license modified, etc, don't hesitate to reach out.
+
--- spiral.py
+++ spiral.py
@@ -1,19 +1,18 @@
 import numpy as np
 from PIL import Image
-import scipy as sp
 
-def encode(array_img, anchor_xy=None, eps=0.2, omega=40):
+def encode(array_img, anchor_xy=None):
     '''
     Encodes an image with a set of parameters which produces a 
     curve giving the impression of the same image by eye.
     
-    The eventual functional form we're aiming for is
+    The eventual functional form we're aiming for is roughly
     
-        X(s) = x(s) + dx(s)*eps*a(s)*sin(w*s).
-        Y(s) = y(s) + dy(s)*eps*a(s)*sin(w*s),
+        X(s) = x(s) + eps*u*sin(omega*s)
+        Y(s) = y(s) + eps*v*sin(omega*s)
         
     where parameter s smoothly interpolates with enough resolution 
-    to draw the curve on top of (x(t), y(t)) (anchor_xy).
+    to draw the curve on top of (x(t), y(t)) (xy).
     
     This function constructs a(t) based on sampling adjacent image intensities 
     near point (x(t), y(t)).
@@ -26,18 +25,11 @@ def encode(array_img, anchor_xy=None, eps=0.2, omega=40):
             choices are made for a trajectory.
 
     Outputs:
-        eps  : float, max amplitude of perturbations
-        w    : float, oscillation frequency.
-        s    : numpy array of floats, monotone increasing from 0 to 1; 
-            expect on the order of length 2*w*n to smoothly 
-            sample oscillations.
-        a(s) : array of floats, functional parameter on [0,1] 
-            associated with the curve. Locally the form is
-            
-                x(s) + dx*eps*a(s)*sin(w*s).
-                y(s) + dy*eps*a(s)*sin(w*s),
-                
-            where (dx,dy) is a local rate of change for the curve.
+        xy : passes the input as output; otherwise passes internally
+            created default.
+        a_t : array of floats, functional parameter on [0,1] 
+            associated with the local amplitude of oscillations.
+    
     '''
     #####################
     #
@@ -47,11 +39,11 @@ def encode(array_img, anchor_xy=None, eps=0.2, omega=40):
     m,n = _shape
     
     # Default curve is spiral r=k*theta with center at center of image.
-    # Choose k and theta such that 10 revolutions happen 
+    # Choose k and theta such that 30 revolutions happen 
     # to hit the closest image boundary.  
     if anchor_xy is None:
         _y0, _x0 = m/2, n/2
-        _t = np.linspace(0, 10*2*np.pi, 10*60)
+        _t = np.linspace(0, 40*2*np.pi, 40*60)
         _k = min(_x0,_y0)/max(_t)
         if n>m:
             _t = _t - np.pi/2 # begin/halt at bottom of circle instead.
@@ -62,13 +54,10 @@ def encode(array_img, anchor_xy=None, eps=0.2, omega=40):
     #
     x,y = anchor_xy
     t = np.linspace(0,1, len(x))
-    ref = max(2*int(omega),1)
-    s = np.linspace(0,1, ref*len(x))
     
     a_t = np.zeros(len(t))
-    #a_s = np.zeros(len(s))
     for k in range(len(t)):
-        # TODO: average neighboring pixels rather than rounding down.
+        # TODO: average neighboring pixels rather than clipping/rounding down.
         i,j = int(anchor_xy[0][k]), int(anchor_xy[1][k])
         
         # in-bounds?
@@ -76,44 +65,105 @@ def encode(array_img, anchor_xy=None, eps=0.2, omega=40):
             a_t[k]=0
         else:
             a_t[k] = array_img[i,j]/255
-        a_s[ref*k] = a_t[k]
     #
-        
-    # linearly interpolate on the finer grid.
-    #for k in range(len(t)-1):
-    #    _th = np.linspace(0,1,ref)
-    #    a_s[ref*k:ref*(k+1)] = (1-_th)*a_s[ref*k] + _th*a_s[ref*(k+1)]
-    return a_t
+    
+    # invert about 0.5 since we want lighter pixels
+    # (larger array values) to have lesser oscillation amplitude.
+    a_t = 1-a_t
+    
+    return anchor_xy,a_t
 #
 
-def resolve(a_t, eps=0.2, omega=40):
+def resolve(anchor_xy, a_t, eps=4, omega=8):
     '''
-    Given a_t, eps, and omega, computes
-        X(s) = x(s) + eps*
+    Given a_t, eps, and omega, computes and outputs roughly
     
+        X(s) = x(s) + eps*a(s)*u*sin(omega*s)
+        Y(s) = y(s) + eps*a(s)*v*sin(omega*s)
+    
+    Inputs:
         eps: float, max amplitude for oscillations. 
         Algorithm works in pixel coordinates; so for tight curves 
         it's recommended 0 < eps < 1 (adjacent pixels are 1 unit apart)
         DEFAULT: 
     omega : float, oscillation frequency for perturbations.
         DEFAULT: 
+    
+    Outputs:
+        X_s, Y_s : numpy arrays of floats to substitute into ax.plot (for example).
     '''
     n = len(a_t)
-    skip = int(2*omega)
+    skip = int(8*omega) # TODO: less ambiguous resolution parameter; non hard-coded
     N = skip*n
     
     a_s = np.zeros(N, dtype=float)
     
-    #for k in range(n):
-    #    a_s[k*skip:(k+1)*skip] = a_
-    pass
+    x,y = anchor_xy
+    t = np.linspace(0,1,n)
     
+    s = np.linspace(0, n*(2*np.pi), N)
+    
+    # linearly interpolate between consecutive (x(t),y(t))
+    x_s,y_s = np.zeros(N, dtype=float),np.zeros(N, dtype=float)
+    for k in range(n):
+        x_s[k*skip] = x[k]
+        y_s[k*skip] = y[k]
+        a_s[k*skip] = a_t[k]
+    #
+    
+    _th = np.linspace(0,1,skip)
+    for k in range(n-1):
+        x_s[k*skip:(k+1)*skip] = (1-_th)*x_s[k*skip] + _th*x_s[(k+1)*skip]
+        y_s[k*skip:(k+1)*skip] = (1-_th)*y_s[k*skip] + _th*y_s[(k+1)*skip]
+        a_s[k*skip:(k+1)*skip] = (1-_th)*a_s[k*skip] + _th*a_s[(k+1)*skip]
+    #
+    maxk = ((N-1)*skip)+1
+    ##
+    
+    # Identify parallel and perp directions relative to the curve; 
+    # oscillations should be perp to the local velocity.
+    # TODO: sort out issues with 0 speed, truncating arrays, etc.
+    x_s = x_s[:maxk]
+    y_s = y_s[:maxk]
+    a_s = a_s[:maxk]
+    
+    dx = np.diff(x_s)
+    dy = np.diff(y_s)
+    dx = np.concat([dx, [dx[-1]]])
+    dy = np.concat([dy, [dy[-1]]])
+    
+    speed = np.sqrt(dx**2 + dy**2)
+    
+    u = -dy/speed
+    v = dx/speed
+    
+    # compute
+    X_s = x_s + (eps*a_s)*u*np.sin(omega*s[:maxk])
+    Y_s = y_s + (eps*a_s)*v*np.sin(omega*s[:maxk])
+    
+    return X_s,Y_s
+#
+
+
 if __name__=="__main__":
     from matplotlib import pyplot as plt
     
-    # TODO: Not clear if this conversion to greyscale works for native CMYK images.
+    # TODO: don't know if this conversion to greyscale works for native CMYK images.
     im = Image.open('Kermit_Drinking_Tea.jpg').convert('L')
     arr = np.array(im)
     
-    a_t = encode(arr)
+    xy,a_t = encode(arr)
+    
+    X_s,Y_s = resolve(xy, a_t)
+    
+    # visualize
+    fig,ax = plt.subplots(1,2, figsize=(10,5), constrained_layout=True)
+    
+    ax[0].matshow(arr, cmap=plt.cm.Greys_r)
+    ax[1].plot(Y_s, -X_s, lw=0.5)
+    ax[1].set(aspect='equal', xticks=[], yticks=[])
+    ax[0].set(xticks=[], yticks=[])
+    
+    fig.show()
+    #fig.savefig('kermie.jpg', bbox_inches='tight')
     
